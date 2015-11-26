9b506062b3f7184a616a9077bbf83c7a5189ad23
Simon Cross <hodgestar@gmail.com>
2015-11-27 00:54:53 +0200

    Correctly server static content.

--- graas/api.py
+++ graas/api.py
@@ -1,22 +1,27 @@
 """ GRaaS API. """
 
+import pkg_resources
+
 from klein import Klein
 
 from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.python.filepath import FilePath
 from twisted.web.static import File
 
 
 class GraasApi(object):
 
     app = Klein()
+    static_path = pkg_resources.resource_filename(__name__, "static")
 
     @app.route('/')
     def home(self, request):
-        return File("./static/index.html")
+        p = FilePath(self.static_path).child("index.html")
+        return p.open().read()
 
     @app.route('/static/', branch=True)
     def static(self, request):
-        return File("./static")
+        return File(self.static_path)
 
     @app.route('/action/press', methods=['POST'])
     def action_press(self, request):
