dd5977244f7499480d79f63f152e66f8f43b06ad
Simon Cross <hodgestar@gmail.com>
2015-11-27 00:26:11 +0200

    Hook up static site via API.

--- graas/api.py
+++ graas/api.py
@@ -3,14 +3,21 @@
 from klein import Klein
 
 from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.web.static import File
 
 
 class GraasApi(object):
 
     app = Klein()
 
-    @app.route('/', methods=['GET'])
-    @inlineCallbacks
-    def get_main_page(self, request):
-        yield
-        returnValue("Hello World!")
+    @app.route('/')
+    def home(self, request):
+        return File("./static/index.html")
+
+    @app.route('/static/', branch=True)
+    def static(self, request):
+        return File("./static")
+
+    @app.route('/action/press', methods=['POST'])
+    def action_press(self, request):
+        return "Button pressed"
